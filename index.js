1c2f8f096e4a38435b34877a841f23be5edfbda8
sotuken19119 <73327418+sotuken19119@users.noreply.github.com>
2020-10-30 16:28:34 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+  
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route ,Switch} from 'react-router-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import End from './End';
+import Start from './Start';
+import NotFound from './404';
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/" exact component={End} />
+          <Route path="/Start"  component={Start} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+    
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
