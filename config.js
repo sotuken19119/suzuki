be1d418de9d48bb5ab6b36371cd5194cca803c55
sotuken19119 <73327418+sotuken19119@users.noreply.github.com>
2020-11-06 15:41:17 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCJvMW5Ke7sV2TPirDgdPvZmvYbnmIvn-o",
+    authDomain: "sotuken2020-b11ea.firebaseapp.com",
+    databaseURL: "https://sotuken2020-b11ea.firebaseio.com",
+    projectId: "sotuken2020-b11ea",
+    storageBucket: "sotuken2020-b11ea.appspot.com",
+    messagingSenderId: "660167068738",
+    appId: "1:660167068738:web:3189a5c62b147f4afdb1db",
+    measurementId: "G-V9X50RW68M"
+  };
